f23b9b029c9a12fd5808d71ae7058c718c0ccd1c
Fidel Remedios Rosado <fiderosado@gmail.com>
2025-01-29 04:43:24 -0500

    First commit to create a page

--- vitest.setup.js
+++
@@ -1,5 +0,0 @@
-import { expect } from 'vitest' // Importa el expect de Vitest
-import * as matchers from '@testing-library/jest-dom/matchers'
-
-// Extiende expect con los matchers de jest-dom
-expect.extend(matchers)
